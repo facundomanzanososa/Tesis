abfa6c984726951d8f07c2f82e61850dab3d8520
Sofexs <paulocontrera97@gmail.com>
2024-02-28 23:23:05 -0300

    Frontend terminado (Mostrar, Agregar y Buscar)

--- Tesis-Front/src/app/component/buscar/buscar.component.ts
+++ Tesis-Front/src/app/component/buscar/buscar.component.ts
@@ -4,7 +4,7 @@ import { Loading, Notify } from 'notiflix';
 
 import { ConsultasService } from 'src/app/services/consultas.service';
 import { Mostrar } from 'src/app/models/mostrar';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-buscar',
   templateUrl: './buscar.component.html',
@@ -20,9 +20,9 @@ export class BuscarComponent {
   form: FormGroup;
 
   constructor(public fb: FormBuilder, private mostrarS: ConsultasService, private router: Router) { 
-    this.form= this.fb.group({
-      id: [''],
-    })
+    this.form = this.fb.group({
+      id: [null, Validators.required] // Agregamos Validators.required para requerir un valor en el campo 'id'
+    });
   }
   ngOnInit(): void {
   }
@@ -30,27 +30,26 @@ export class BuscarComponent {
 
 
   buscarproducto(): void {
+      console.log(this.form.value.id);
+      
     Loading.standard('Cargando...');    
     this.mostrarS.individual(this.form.value.id).subscribe(
       data =>{
-        this.producto = data;
-        console.log(data);
-        this.mostrar=true;
-        
+        if ((Array.isArray(data) && data.length === 0)) {
+        Notify.failure('Producto no disponible');
 
+          this.mostrar = false;
+        } else {
+          this.producto = data;
+          this.mostrar = true;
+        }
         Loading.remove();
-
       }, err =>{
         Notify.failure('¡Ups! Algo salió mal');
         this.router.navigate(['']);
         this.mostrar=false;
-
         Loading.remove();
-
       }
     )
-
-    
   }
-
 }
